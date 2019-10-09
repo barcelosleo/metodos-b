c4ac00d78c2eefb02613af7d46496be54ab5e415
Leonardo Barcelos <leobarcelos12@gmail.com>
2019-10-09 20:43:18 -0300

    Novas alterações

--- Trabalho 1/f2.py
+++ Trabalho 1/f2.py
@@ -4,12 +4,10 @@ import sys
 
 mi = 1.4e4 # Taxa de consumo de combustível [Kg/s]
 M = 2.7e6 # Massa total do foguete [Kg]
-# M = 3.7e6 # Massa total do foguete [Kg]
-# M = 100 # Massa total do foguete [Kg]
 Mc0 = M * 0.94 # Massa inicial de combustível [Kg] {94% da massa de combustível}
 M0 = M - Mc0 # Massa do foguete sem combustível [Kg]
 g = 9.8 # aceleração da gravidade [m/s²]
-Vc = 2.7e3 # módulo da velocidade do combustível [m/s]
+Vc = 3e3 # módulo da velocidade do combustível [m/s]
 Vf0 = 0
 
 def massaInstantaneaFoguete(t):
@@ -35,7 +33,7 @@ def momento(t, mf, mc, Vf):
 def energia(m, v, h):
     return ((m * v ** 2) / 2) - (m * g * h)
 
-def euler(h, t_max, f):
+def euler(h, f):
     Vf = Vf0
 
     l_vf = [Vf]
@@ -47,7 +45,7 @@ def euler(h, t_max, f):
     i = 0
     t = 0
     mf = M
-    while mf > M0 and t < t_max:
+    while mf > M0:
         i += 1
         t = float(i * h)
 
@@ -70,7 +68,7 @@ def euler(h, t_max, f):
         'e': l_h,
     }
 
-def rk2(h, t_max, f):
+def rk2(h, f):
     Vf = Vf0
 
     l_vf = [Vf]
@@ -82,7 +80,7 @@ def rk2(h, t_max, f):
     i = 0
     t = 0
     mf = M
-    while mf > M0 and t < t_max:
+    while mf > M0:
         i += 1
         t = float(i * h)
 
@@ -107,7 +105,7 @@ def rk2(h, t_max, f):
         'e': l_h,
     }
 
-def rk4(h, t_max, f):
+def rk4(h, f):
     Vf = Vf0
 
     l_vf = [Vf]
@@ -119,7 +117,7 @@ def rk4(h, t_max, f):
     i = 0
     t = 0
     mf = M
-    while mf > M0 and t < t_max:
+    while mf > M0:
         i += 1
         t = float(i * h)
 
@@ -147,30 +145,104 @@ def rk4(h, t_max, f):
     }
 
 def main():
-    h = 0.1
-    t_max = 1
-    e = euler(h, t_max, f)
-    r2 = rk2(h, t_max, f)
-    r4 = rk4(h, t_max, f)
+    h = 0.001
+    e = euler(h, f)
+    r2 = rk2(h, f)
+    r4 = rk4(h, f)
 
-    # analitico = {
-    #     't': e['t'],
-    #     'Vf': [velocidadeAnalitica(t) for t in e['t']]
-    # }
+    analitico = {
+        't': e['t'],
+        'Vf': [velocidadeAnalitica(t) for t in e['t']]
+    }
 
-    # plt.plot(e['t'], e['Vf'])
-    # plt.plot(r2['t'], r2['Vf'])
-    # plt.plot(r4['t'], r4['Vf'])
-    # plt.plot(analitico['t'], analitico['Vf'])
+    plt.subplot(1, 2, 1)
+    plt.plot(e['t'], e['Vf'])
+    plt.plot(r2['t'], r2['Vf'])
+    plt.plot(r4['t'], r4['Vf'])
+    plt.plot(analitico['t'], analitico['Vf'])
+    plt.ylabel('V(m/s)')
+    plt.xlabel('t(s)')
 
-    # plt.legend(['Euler', 'RK2', 'RK4', 'Analitico'])
+    plt.subplot(1, 2, 2)
+    plt.plot(e['t'], e['h'])
+    plt.plot(r2['t'], r2['h'])
+    plt.plot(r4['t'], r4['h'])
+    plt.ylabel('Altura(m)')
+    plt.xlabel('t(s)')
 
-    # plt.show()
-
-    plt.plot(e['t'], e['m'])
-    plt.plot(r2['t'], r2['m'])
-    plt.plot(r4['t'], r4['m'])
     plt.legend(['Euler', 'RK2', 'RK4'])
+
+    plt.show()
+
+    p = 4
+
+    valor_analitico = velocidadeAnalitica(e['t'][-1])
+
+    legends = []
+
+    l_h = []
+    l_e_e = []
+    l_e_r2 = []
+    l_e_r4 = []
+
+    for j in range(p):
+        h = 1 * 10 ** -j
+
+        e = euler(h, f)
+        r2 = rk2(h, f)
+        r4 = rk4(h, f)
+
+        l_h.append(h)
+
+        l_e_e.append(abs(valor_analitico - e['Vf'][-1]))
+        l_e_r2.append(abs(valor_analitico - r2['Vf'][-1]))
+        l_e_r4.append(abs(valor_analitico - r4['Vf'][-1]))
+
+    plt.plot(l_h, l_e_e, '.-')
+    plt.plot(l_h, l_e_r2, '.-')
+    plt.plot(l_h, l_e_r4, '.-')
+    plt.legend(['Euler', 'RK2', 'RK4'])
+    plt.title('Erros')
+
+    plt.ylabel(r'$ |V(' + str(e['t'][-1]) + ') - V_{analitico}(' + str(e['t'][-1]) +')| $')
+    plt.xlabel('h')
+
+    plt.xscale('log')
+    plt.yscale('log')
+
+    plt.show()
+
+
+    for j in range(1, p):
+        h = 1 * 10 ** -j
+
+        e = euler(h, f)
+        r2 = rk2(h, f)
+        r4 = rk4(h, f)
+
+        erro_e = []
+        erro_r2 = []
+        erro_r4 = []
+
+        legends.append(f'Euler h = {1 * 10 ** -j}')
+        legends.append(f'RK2 h = {1 * 10 ** -j}')
+        legends.append(f'RK4 h = {1 * 10 ** -j}')
+
+        for i in range(len(e['Vf'])):
+            erro_e.append(abs(e['Vf'][i] - analitico['Vf'][i]))
+            erro_r2.append(abs(r2['Vf'][i] - analitico['Vf'][i]))
+            erro_r4.append(abs(r4['Vf'][i] - analitico['Vf'][i]))
+
+        plt.plot(e['t'], erro_e)
+        plt.plot(e['t'], erro_r2)
+        plt.plot(e['t'], erro_r4)
+
+
+    plt.legend(legends)
+    plt.yscale('log')
+    plt.ylabel(r'$ | V_n - V_{analitico} | $')
+    plt.xlabel('h')
+    plt.title('Erros')
 
     plt.show()
 
